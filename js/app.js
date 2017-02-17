cd3f2e3362ff099545b9f0ae899746cadc38f44d
11lopez.jessica@gmail.com <11lopezjessica@gmail.com>
2017-02-16 18:08:07 -1000

    2/16/17

--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
-function hi() {
+/*function hi() {
   var changeDiv = document.getElementById("fortune-cookie-text");
-  changeDiv.innerHTML = "Aloha";
+  changeDiv.innerHTML = "You will be " + i;
   console.log(changeDiv);
+}*/
+
+var fortunes = ["You will be happy", "Life's gonna bite you on the butt", "Life will be difficult, but it'll be worth it in the end", "You will be rich", "You're gonna be poooor"];
+var fortune = "";
+var saidAll=false;
+
+function hi() {
+	if (saidAll==false) {
+		var button = document.getElementById('onlyButton');
+		var box = document.getElementById('fortune-cookie-text');
+		var list = document.getElementById('list');
+		var li = document.getElementById('li');
+		button.innerHTML = "Keep Clicking Me!";
+
+		if(fortune!= "") {
+			var node = "";
+			var textnode = document.createTextNode("fortune");
+			li.appendChild(textnode);
+			document.getElementById('li').appendChild(mode);
+
+		}
+		if (fortunes.length>0) {
+			var num;
+			num - Math.floor(Math.random()*fortunes.length);
+			fortune = fortunes[num];
+
+			fortunes.splice(num, 1);
+			box.innerHTML = + fortunes;
+		}
+		else {
+			button.innerHTML = "You ran out of fortunes, oops";
+			saidAll=true;
+		}
+	}
 }
